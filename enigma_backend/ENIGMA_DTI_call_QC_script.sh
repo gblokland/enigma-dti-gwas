519d631493de403966cb15dae0ac33ae5b19fcef
Gabriella Blokland <gblokland@145.127.EARLY-REGISTRATION.of.SURFnet.invalid>
2025-10-29 18:02:24 +0100

    Update enigma_backend scripts

--- enigma_backend/ENIGMA_DTI_call_QC_script.sh
+++ enigma_backend/ENIGMA_DTI_call_QC_script.sh
@@ -13,53 +13,69 @@
 #%% enigma.dtigenetics@gmail.com
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Cohort=DMS
-echo $Cohort
-ENIGMA_DTI_GWAS_dir=~/enigmaDTI/enigma_dti_gwas
+COHORT=DMS
+echo $COHORT
+#ENIGMA_DTI_GWAS_dir=~/enigmaDTI/enigma_dti_gwas
+ENIGMA_DTI_GWAS_dir=~/enigma/DTIgenetics
 echo $ENIGMA_DTI_GWAS_dir
 cd $ENIGMA_DTI_GWAS_dir
 
-cp ~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_call_QC_script.sh ./
-cp ~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_ALL-GB.R ./
-cp ~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_ALL.R ./
-cp ~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_stats_demographics.R ./
-cp ~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_stats_demographics-simple-GB.R ./
+#cp ~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_call_QC_script.sh ./
+#cp ~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_ALL-GB.R ./
+#cp ~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_ALL.R ./
+#cp ~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_stats_demographics.R ./
+#cp ~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_stats_demographics-simple-GB.R ./
 
 
-wget "~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_call_QC_script.sh"
-wget "~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_ALL-GB.R"
-wget "~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_ALL.R"
-wget "~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_stats_demographics.R"
-wget "~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_stats_demographics-simple-GB.R"
+#wget "~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_call_QC_script.sh"
+#wget "~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_ALL-GB.R"
+#wget "~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_ALL.R"
+#wget "~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_stats_demographics.R"
+#wget "~/enigma.dtigenetics\@gmail.com\ -\ Google\ Drive/My\ Drive/ENIGMA_DTI_GWAS_genetics_taskforce/ENIGMA_DTI_association_protocols/ENIGMA_DTI_plots_stats_demographics-simple-GB.R"
 
+git clone git@github.com:gblokland/enigma-dti-gwas.git SCRIPTS
 
 #Rscript ENIGMA_DTI_plots_qc_script.R \
 Rscript ENIGMA_DTI_plots_ALL-GB.R \
---site ${Cohort} \
---outD "${ENIGMA_DTI_GWAS_dir}/QC_ENIGMA/" \
---CSVfile "${ENIGMA_DTI_GWAS_dir}/combinedROItable_FA.csv" \
+--cohort ${COHORT} \
+--outDir "${ENIGMA_DTI_GWAS_dir}/QC_ENIGMA/" \
+--CSVfile "${ENIGMA_DTI_GWAS_dir}/pheno_covar/combinedROItable_FA.csv" \
 --rois "AverageFA;BCC;GCC;SCC;CC;CGC;CGH;CR;EC;FX;FXST;IC;IFO;PTR;SFO;SLF;SS;UNC;CST;ACR;ALIC;PCR;PLIC;RLIC;SCR;ACR.L;ACR.R;ALIC.L;ALIC.R;CGC.L;CGC.R;CGH.L;CGH.R;CR.L;CR.R;CST.L;CST.R;EC.L;EC.R;FX.ST.L;FX.ST.R;IC.L;IC.R;IFO.L;IFO.R;PCR.L;PCR.R;PLIC.L;PLIC.R;PTR.L;PTR.R;RLIC.L;RLIC.R;SCR.L;SCR.R;SFO.L;SFO.R;SLF.L;SLF.R;SS.L;SS.R;UNC.L;UNC.R" \
---outPDF "${Cohort}_ENIGMA_DTI_allROI_histograms.pdf" \
---outTXT "${Cohort}_ENIGMA_DTI_allROI_stats.txt"
+--outPDF "${COHORT}_ENIGMA_DTI_allROI_histograms.pdf" \
+--outTXT "${COHORT}_ENIGMA_DTI_allROI_stats.txt"
 
 ls ${ENIGMA_DTI_GWAS_dir}/QC_ENIGMA/
 
-#--rois "AverageFA;BCC;GCC;SCC;CC;CGC;CGH;CR;EC;FX;FXST;IC;IFO;PTR;SFO;SLF;SS;UNC;CST;ACR;ALIC;PCR;PLIC;RLIC;SCR;ACR-L;ACR-R;ALIC-L;ALIC-R;CGC-L;CGC-R;CGH-L;CGH-R;CR-L;CR-R;CST-L;CST-R;EC-L;EC-R;FX/ST-L;FX/ST-R;IC-L;IC-R;IFO-L;IFO-R;PCR-L;PCR-R;PLIC-L;PLIC-R;PTR-L;PTR-R;RLIC-L;RLIC.-R;SCR-L;SCR-R;SFO-L;SFO-R;SLF-L;SLF-R;SS-L;SS-R;UNC-L;UNC-R" \
 
+Rscript ${ENIGMA_DTI_GWAS_dir}/SCRIPTS/enigma_backend/ENIGMA_DTI_plots_All_inclCohensd.R \
+--cohort ${COHORT} \
+--covarFILE "${ENIGMA_DTI_GWAS_dir}/pheno_covar/${COHORT}_enigma_dti_gwas.covar" \
+--phenoFILE "${ENIGMA_DTI_GWAS_dir}/pheno_covar/${COHORT}_enigma_dti_gwas.pheno" \
+--rois "AverageFA;BCC;GCC;SCC;CC;CGC;CGH;CR;EC;FX;FXST;IC;IFO;PTR;SFO;SLF;SS;UNC;CST;ACR;ALIC;PCR;PLIC;RLIC;SCR;ACR.L;ACR.R;ALIC.L;ALIC.R;CGC.L;CGC.R;CGH.L;CGH.R;CR.L;CR.R;CST.L;CST.R;EC.L;EC.R;FX.ST.L;FX.ST.R;IC.L;IC.R;IFO.L;IFO.R;PCR.L;PCR.R;PLIC.L;PLIC.R;PTR.L;PTR.R;RLIC.L;RLIC.R;SCR.L;SCR.R;SFO.L;SFO.R;SLF.L;SLF.R;SS.L;SS.R;UNC.L;UNC.R" \
+--outDir "${ENIGMA_DTI_GWAS_dir}/QC_ENIGMA/" \
+--outPDF "${COHORT}_ENIGMA_DTI_allROI_histograms.pdf" \
+--outTXT "${COHORT}_ENIGMA_DTI_allROI_stats.txt"
 
 
 Rscript ENIGMA_DTI_plots_stats_demographics.R \
---site ${Cohort} \
---outD "${ENIGMA_DTI_GWAS_dir}/QC_ENIGMA/" \
---CSVfile "${ENIGMA_DTI_GWAS_dir}/Covariates.csv" \
---outPDF "${Cohort}_ENIGMA_DTI_Age_histograms.pdf" \
---outTXT "${Cohort}_ENIGMA_DTI_Age_stats.txt"
+--cohort ${COHORT} \
+--CSVfile "${ENIGMA_DTI_GWAS_dir}/pheno_covar/Covariates.csv" \
+--outDir "${ENIGMA_DTI_GWAS_dir}/QC_ENIGMA/" \
+--outPDF "${COHORT}_ENIGMA_DTI_Age_histograms.pdf" \
+--outTXT "${COHORT}_ENIGMA_DTI_Age_stats.txt"
 
 
 Rscript ENIGMA_DTI_plots_stats_demographics-simple-GB.R \
---site ${Cohort} \
---outD "${ENIGMA_DTI_GWAS_dir}/QC_ENIGMA/" \
---CSVfile "${ENIGMA_DTI_GWAS_dir}/Covariates.csv" \
---outPDF "${Cohort}_ENIGMA_DTI_Age_histograms.pdf" \
---outTXT "${Cohort}_ENIGMA_DTI_Age_stats.txt"
+--cohort ${COHORT} \
+--CSVfile "${ENIGMA_DTI_GWAS_dir}/pheno_covar/Covariates.csv" \
+--outDir "${ENIGMA_DTI_GWAS_dir}/QC_ENIGMA/" \
+--outPDF "${COHORT}_ENIGMA_DTI_Age_histograms.pdf" \
+--outTXT "${COHORT}_ENIGMA_DTI_Age_stats.txt"
+
+Rscript ENIGMA_DTI_phenotype_and_covariate_sumstats.R \
+--cohort ${COHORT} \
+--CSVfile "${ENIGMA_DTI_GWAS_dir}/pheno_covar/Covariates.csv" \
+--outDir "${ENIGMA_DTI_GWAS_dir}/QC_ENIGMA/" \
+--outPDF "${COHORT}_ENIGMA_DTI_Age_histograms.pdf" \
+--outTXT "${COHORT}_ENIGMA_DTI_Age_stats.txt"
 
