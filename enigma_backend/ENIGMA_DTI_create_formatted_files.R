81c0b4f648d70c5feb662b1fc74ef09cb5d562a0
Gabriella Blokland <gblokland@FHMLSPWKS0099.local>
2025-11-01 02:24:25 +0100

    Update enigma_backend scripts

--- enigma_backend/ENIGMA_DTI_create_formatted_files.R
+++ enigma_backend/ENIGMA_DTI_create_formatted_files.R
@@ -387,7 +387,7 @@ for (s in c(1:3)) {
           cat(paste('The standard deviation of column', VarNames[l], 'is zero. Therefore, the column will be removed.'))
           columnnames = colnames(FullInfoFile)
           #FullInfoFile=FullInfoFile[,-which(columnnames==VarNames[l])]
-          FullInfoFile[[VarNames[l]]] <- NULL  # remove the column safely
+          FullInfoFile[,VarNames[l]] <- NULL  # remove the column safely
           drp=drp+1
         }
       }
